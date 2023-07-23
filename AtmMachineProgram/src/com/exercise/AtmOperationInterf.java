ac4246d8a96d079ea109860d595643bf3a0cb6bc
sikandarsayyad <127669924+sikandarsayyad@users.noreply.github.com>
2023-07-23 18:18:34 +0530

    Add files via upload

---
+++ AtmMachineProgram/src/com/exercise/AtmOperationInterf.java
@@ -0,0 +1,10 @@
+package com.exercise;
+
+public interface AtmOperationInterf {
+	
+	public void viewBalance();
+	public void withdrawAmount(double withdrawAmount);
+	public void depositAmount(double depositAmount);
+	public void viewMiniStatement();
+
+}
